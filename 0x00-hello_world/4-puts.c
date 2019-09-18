455118f85c740077b574ca83498770d85152c5c5
christinepang1 <christinezpang@gmail.com>
2019-09-18 21:40:19 +0000

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: Uses puts
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+}
