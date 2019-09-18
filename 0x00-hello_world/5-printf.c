1621fbec45810e7553c68b5fc04ac2db42060e00
christinepang1 <christinezpang@gmail.com>
2019-09-18 21:21:55 +0000

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * Description: Use printf
+ * Return: always 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
