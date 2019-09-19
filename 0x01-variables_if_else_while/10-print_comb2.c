47f566eab2cc942c5985585b8ddeef3d0bc6d5d8
christinepang1 <christinezpang@gmail.com>
2019-09-19 23:47:42 +0000

    List double digits

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Descriptions: Write a program that prints the numbers from 00 to 99
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i;
+
+	while (i < 100)
+	{
+		putchar((i % 10) + '0');
+		putchar((i % 10) + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
