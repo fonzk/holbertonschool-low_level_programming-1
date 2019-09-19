c2b517c24262445bda8527c2a3553f94cd4ab712
christinepang1 <christinezpang@gmail.com>
2019-09-19 21:09:08 +0000

    Print alphabet minus q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Descriptions: Print alphabet minus q and e
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a < 'z')
+	{
+		if (a != 'e' && a != 'q')
+			putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
