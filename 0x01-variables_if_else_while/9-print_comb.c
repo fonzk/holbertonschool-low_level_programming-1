786f39f8f044970a4e47422b48103df473c7f7f9
christinepang1 <christinezpang@gmail.com>
2019-09-19 22:48:10 +0000

    Print string

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Descriptions: program prints all possible combo of single-digit numbers.
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
