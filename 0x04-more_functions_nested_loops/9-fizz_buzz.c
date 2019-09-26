af9b89f5640689b53f63a510ad09b59d35ecd61a
christinepang1 <christinezpang@gmail.com>
2019-09-26 20:38:38 +0000

    Print fizz and buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,9 +2,9 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * main - entry point
+ * Descriptions: Print fizz and buzz
+ * Return: 0 if successful
  */
 int main(void)
 {
@@ -16,10 +16,12 @@ int main(void)
 			printf("Fizz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
+		else if (i == 100)
+			printf("Buzz");
 		else if (i % 15 == 0)
 			printf("FizzBuzz ");
 		else
-			printf("%i ",i);
+			printf("%i ", i);
 	}
 	printf("\n");
 	return (0);
