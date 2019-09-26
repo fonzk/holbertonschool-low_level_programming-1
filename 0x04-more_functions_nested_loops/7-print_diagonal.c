54bcadc465a4be4ae3d9bb3198c4bf669c1c8738
christinepang1 <christinezpang@gmail.com>
2019-09-26 19:02:10 +0000

    Draws a diagonal line on the terminal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - entry point to print spaces and slashes
+ * @n: integer through the function
+ */
+void print_diagonal(int n)
+{
+	int sla, spa;
+
+	if (n < 0)
+		_putchar('\n');
+	else
+	{
+	for (sla = 0; sla < n; sla++)
+	{
+		for (spa = 0; spa < sla; spa++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	}
+}
