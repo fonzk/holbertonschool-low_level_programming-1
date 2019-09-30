48e500ac8c6cf16389113e099ecffaeefd76110f
christinepang1 <christinezpang@gmail.com>
2019-09-30 23:35:53 +0000

    Swap 2 integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swap value of 2 integers
+ * @a: first integer to swap
+ * @b: second integer to swap
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+
+}
