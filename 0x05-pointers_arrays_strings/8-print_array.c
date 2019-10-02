db144a4692e22f47fdbab2c61217d2707ba4d0ea
christinepang1 <christinezpang@gmail.com>
2019-10-02 05:15:32 +0000

    Print contents of array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - print n elements
+ * @a: array
+ * @n: integer
+ */
+void print_array(int *a, int n)
+{
+	int count;
+
+	while (count < n)
+	{
+		printf("%d", a[count]);
+		if (count != n - 1)
+		{
+			printf(", ");
+		}
+		count++;
+        }
+	printf("\n");
+}
