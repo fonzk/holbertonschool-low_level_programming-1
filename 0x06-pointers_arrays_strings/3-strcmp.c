854181693bbb5a5d34227fcd1c536bb71d13c5ce
christinepang1 <christinezpang@gmail.com>
2019-10-03 20:38:00 +0000

    Compare 2 strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strcmp - Function that compares 2 strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: 0 if 0, negative integer, or positive integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0' || *s2 == '\0')
+			break;
+		s1++;
+		s2++;
+	}
+	if (*s1 == '\0' && *s2 == '\0')
+		return (0);
+	else if (*s1 < *s2)
+		return (*s2 - *s1);
+	else
+		return (*s2 - *s1);
+}
