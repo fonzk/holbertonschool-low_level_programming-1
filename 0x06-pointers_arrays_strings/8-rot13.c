454266a0dbeafd6c0b214a8a7fdd3005cfb2273c
christinepang1 <christinezpang@gmail.com>
2019-10-04 06:28:33 +0000

    ROT13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * rot-13 - encode a string
+ * @s: string
+ * Return: encoded string
+ */
+char *rot13(char *s)
+{
+	int i;
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	i = 0;
+
+	while (str[i])
+	{
+		while ((str[i] != input[i] && input[i])
+			{
+				i++;
+			}
+			while[input[i]
+			      {
+				      output[i] = input[i];
+				      i++;
+			      }
+	}
+			return (s);
+}
