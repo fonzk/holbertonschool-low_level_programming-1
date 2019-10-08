f155b5dae96a392bc44c3cb906dc162503af7d0f
christinepang1 <christinezpang@gmail.com>
2019-10-08 06:21:17 +0000

    Print diagnol

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Function that prints the sum of the two diagonals
+ * of a square matrix of integers
+ * @a: array
+ * @size: size of matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, d1, d2;
+
+	i = 0;
+	j = 0;
+        d1 = 0;
+	d2 = 0;
+
+	while (i < size)
+	{
+		d1 += *(a + i);
+		i++;
+		a += size;
+	}
+
+	a -= size;
+	while (j < size)
+	{
+		d2 += *(a+j);
+		j++;
+		a -= size;
+	}
+	printf("%d, %d\n",d1, d2);
+}
