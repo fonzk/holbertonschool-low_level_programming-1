3b97a1e733cc7b28322ebb792e6f6de1d7f79a9f
christinepang1 <christinezpang@gmail.com>
2019-10-10 03:27:07 +0000

    Function that checks if the number is a prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -16,15 +16,15 @@ int is_prime_number(int n)
  * helper - runs the checks to determine if it is a prime
  * @n: number to check
  * @i: counter
- * Return: 1 if prime, 0 otherwise 
+ * Return: 1 if prime, 0 otherwise
  */
 
 int helper(int n, int i)
 {
 	if (n == i)
-		return 1;
+		return (1);
 	if ((n % i) != 0)
-		return (helper(n, i+1));
+		return (helper(n, i + 1));
 	if ((n % i) == 0)
 		return (0);
 	if (n < i)
