860d0315305a80f1373ce47d75e34802245745f2
christinepang1 <christinezpang@gmail.com>
2019-10-11 21:21:11 +0000

    A

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,11 +14,9 @@ int _atoi(char *s)
        	while ((*s < '0' || *s > '9') && *s != 0)
        	{
        		if (*s == '-')
-       			sign = -sign;
+       			sign *= -1;
        		s++;
 	}
-	if (*s == 0)
-		return (0);
 
        	while ((*s >= '0' && *s <= '9') && *s != 0)
        	{
