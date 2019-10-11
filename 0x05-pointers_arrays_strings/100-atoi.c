f66d833e7958253baa1b4f090c7f70fda788afd1
christinepang1 <christinezpang@gmail.com>
2019-10-11 21:42:57 +0000

    Atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,11 +8,10 @@
 
 int _atoi(char *s)
 {
-	int value = 0;
+	unsigned int value = 0;
 	int sign = 1;
 
-       	do
-	{
+	do {
 		if (*s == '-')
 		{
 			sign = -sign;
@@ -23,10 +22,10 @@ int _atoi(char *s)
 		}
 		else if ((*s >= '0' && *s <= '9') && *s != 0)
 		{
-      	       		value *= 10;
-       	       		value += (*s - '0');
+			value *= 10;
+			value += (*s - '0');
 		}
-       	}while (*s++);
+	} while (*s++);
 
 	return (value * sign);
 }
