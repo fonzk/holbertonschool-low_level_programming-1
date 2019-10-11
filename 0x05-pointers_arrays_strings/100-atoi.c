7d0057bcac38092d401e21f6181986e64f57bf78
christinepang1 <christinezpang@gmail.com>
2019-10-11 20:31:32 +0000

    Return integers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to convert
+ * Return: integers, 0 if none
+ */
+
+int _atoi(char *s)
+{
+	int value = 0;
+	int sign = 1;
+
+	while (*s < '0' || *s > '9')
+	{
+		if (*s == '-')
+			sign = -1;
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		if (value >= 0)
+		{
+			value *= 10;
+			value += (int) (*s - '0');
+			s++;
+		}
+		else
+		{
+			value *= 10;
+			value += (int) (*s - '0');
+			s++;
+		}
+	}
+	return (value * sign);
+}
