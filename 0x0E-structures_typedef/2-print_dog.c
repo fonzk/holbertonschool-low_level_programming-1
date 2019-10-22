8960864baf5e55774a57dad0f7fcb967d0f8f1e2
christinepang1 <christinezpang@gmail.com>
2019-10-22 00:17:42 +0000

    Fixed float

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,11 +13,10 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
-
-	if (!d->age)
-		printf("Age: (nil)\n");
 	else
-		printf("Age: %f\n", d->age);
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
