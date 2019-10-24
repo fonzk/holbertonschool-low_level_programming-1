fee9a802cb44766b6509883bbaed2cb2c62bc3b2
christinepang1 <christinezpang@gmail.com>
2019-10-24 02:26:29 +0000

    Perform math operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - function that adds
+ * @a: first numner
+ * @b: second number
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function that subtracts
+ * @a: first number
+ * @b: second numner
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function that multiplies
+ * @a: first number
+ * @b: second number
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function that divides
+ * @a: first number
+ * @b: second number
+ * Return: a divided by by b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - function that gets the remainder
+ * @a: first number
+ * @b: second number
+ * Return: remainder of a divided by b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+		return (a % b);
+}
