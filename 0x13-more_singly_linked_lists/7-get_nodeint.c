b02b47be89dad07b393a696b1c5fe1af73b1aa81
christinepang1 <christinezpang@gmail.com>
2019-11-06 00:21:41 +0000

    Added NULL for head condition

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,6 +13,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 
 	temp = head;
+
+	if (head == NULL)
+		return (NULL);
+
 	for (i = 0; i < index; i++)
 		temp = temp->next;
 	if (i != index)
