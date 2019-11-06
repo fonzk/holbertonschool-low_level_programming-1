5fa526e1d20915e2b0e6269f07f90e540af15eef
christinepang1 <christinezpang@gmail.com>
2019-11-06 00:24:31 +0000

    Tried NULL condition within loop

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,11 +14,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	temp = head;
 
-	if (head == NULL)
-		return (NULL);
-
 	for (i = 0; i < index; i++)
+	{
+		if (temp == NULL)
+			return (NULL);
 		temp = temp->next;
+	}
+
 	if (i != index)
 		return (NULL);
 
