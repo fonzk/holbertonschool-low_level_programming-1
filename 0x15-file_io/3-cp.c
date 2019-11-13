7e5c4dfc84226024167887aa0bb3decbad49a37f
christinepang1 <christinezpang@gmail.com>
2019-11-13 01:55:52 +0000

    Edge cases

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,20 @@
 #include "holberton.h"
+/**
+ * void - entry
+ * @fie: file name
+ */
+void _close(int file)
+{
+	int f;
+
+	f = close(file);
+
+	if (f == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
+		exit(100);
+	}
+}
 
 /**
  * main - entry
@@ -8,7 +24,29 @@
  */
 int main(int argc, char *argv[])
 {
+	int filefrom, fileto;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	filefrom = open(argv[1], O_RDONLY);
+	if (filefrom == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fileto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fileto == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+		exit(99);
+	}
 
 
+	close(filefrom);
+	close(fileto);
 
 }
