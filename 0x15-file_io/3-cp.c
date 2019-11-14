8744bb77c83b4ccdffe441fc844aadd9073a88ff
christinepang1 <christinezpang@gmail.com>
2019-11-14 00:02:04 +0000

    Uploaded

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,22 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
-
-/**
- * _close - close program
- * @file: file name
- */
-void _close(int file)
-{
-	int f;
-
-	f = close(file);
-
-	if (f == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
-		exit(100);
-	}
-}
+#define BUFF 1024
 
 /**
  * main - entry
@@ -24,11 +8,11 @@ void _close(int file)
  * @argv: arguments
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int filefrom, fileto, fw;
-	ssize_t count;
-	char buffer[1024];
+/*file descriptors, one to read from one to write */
+	ssize_t filefrom, fileto, wc, rc, cc, cd;
+	char buffer[BUFF];
 
 	if (argc != 3)
 	{
@@ -42,21 +26,42 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+/* order does not matter here. leading 0 here is octal */
 	fileto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fileto == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
 		exit(99);
 	}
+/* loop to copy from file to to file */
+	do{
+		rc = read(filefrom, buffer, BUFF);
+		if (rc == -1)
+			return (-1);
+/* not 1024 because that's not always the case. don't want to write more than
+   you read */
+		wc = write(fileto, buffer, rc);
+/* check for write is special */
+/* need to write same number of char as you read */
+		if (wc == -1 || wc != rc)
+			return (-1);
 
+/* how do you know when there is nothing left to read */
+/* like if you print 500 bytesl you've got the leftovers */
+	}while(rc == BUFF);
 
-/* need to check if read works here */ 
-	while ((count = read(filefrom, buffer, 1024)) != 0)
-		fw = write(fileto, buffer, count);
-
-
-	_close(filefrom);
-	_close(fileto);
+	cc = close(filefrom);
+	if (cc == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close  %s", argv[1]);
+		exit(100);
+	}
+	cd = close(fileto);
+	if (cd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close  %s", argv[2]);
+		exit(100);
+	}
 
 	return (0);
 }
