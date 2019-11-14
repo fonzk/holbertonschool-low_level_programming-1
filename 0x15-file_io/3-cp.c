aab9ed191808d9ea9be7db76cd4cb3555c401fd0
christinepang1 <christinezpang@gmail.com>
2019-11-14 04:05:02 +0000

    Corrected exit statements

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,6 @@
  */
 int main(int argc, char **argv)
 {
-/*file descriptors, one to read from one to write */
 	ssize_t filefrom, fileto, wc, rc, cc, cd;
 	char buffer[BUFF];
 
@@ -26,29 +25,30 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-/* order does not matter here. leading 0 here is octal */
+
 	fileto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fileto == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
 		exit(99);
 	}
-/* loop to copy from file to to file */
-	do{
+
+	do {
 		rc = read(filefrom, buffer, BUFF);
 		if (rc == -1)
-			return (-1);
-/* not 1024 because that's not always the case. don't want to write more than
-   you read */
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		wc = write(fileto, buffer, rc);
-/* check for write is special */
-/* need to write same number of char as you read */
-		if (wc == -1 || wc != rc)
-			return (-1);
 
-/* how do you know when there is nothing left to read */
-/* like if you print 500 bytesl you've got the leftovers */
-	}while(rc == BUFF);
+		if (wc == -1 || wc != rc)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+			exit(99);
+		}
+
+	} while (rc == BUFF);
 
 	cc = close(filefrom);
 	if (cc == -1)
