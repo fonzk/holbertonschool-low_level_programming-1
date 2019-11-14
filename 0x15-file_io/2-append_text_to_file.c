6ab2e727f4c0fcfe112e9b414d80ce6bd5d70711
christinepang1 <christinezpang@gmail.com>
2019-11-14 03:48:40 +0000

    Changed condition

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,16 +18,18 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
-		return (1);
 
-	len = 0;
-	while (text_content[len])
-		len++;
+	if (text_content != NULL)
+	{
+		len = 0;
+		while (text_content[len])
+			len++;
+	}
 
 	wr = write(fd, text_content, len);
-		if (wr == -1)
-			return (-1);
+	if (wr == -1)
+		return (-1);
+
 	close(fd);
 	return (1);
 }
