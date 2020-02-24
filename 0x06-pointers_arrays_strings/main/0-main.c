90bea788906e0aac5e49cc8499ab79a4b0c9e4aa
christinepang1 <christinezpang@gmail.com>
2020-02-24 09:15:41 +0000

    Added main files

---
+++ 0x06-pointers_arrays_strings/main/0-main.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *p;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    p = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", p);
+    return (0);
+}
+julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra 0-main.c 0-strcat.c -o 0-strcat
+julien@ubuntu:~/0x06$ ./0-strcat 
+Hello 
+World!
+Hello World!
+World!
+Hello World!
+julien@ubuntu:~/0x06$ 
